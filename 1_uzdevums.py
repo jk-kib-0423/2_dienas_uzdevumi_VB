1ff214b0648a1ee33507b87ed6e1c669835add7b
vabulsb <vabulsb@inbox.lv>
2023-05-29 18:38:12 +0300

    pievienots uzdevums

--- 1_uzdevums.py
+++ 1_uzdevums.py
@@ -1,1 +1,13 @@
-fails
+# Darbs ar failiem
+
+faila_nosaukums = "dati.txt"
+
+# režīmi: r - lasīšana, w - rakstīšana (pārraksta izdzēšot saturu), a - pievieno faila galā
+with open(faila_nosaukums, "w", encoding="utf-8") as fails:
+    fails.write("Pirmā testa rindiņa.\n")
+    fails.write("Otra testa rindiņa.\n")
+
+
+with open(faila_nosaukums, "a", encoding="utf-8") as fails:
+    fails.write("Trešā rindiņa, kā papildinājums.\n")
+    fails.write("Ceturtā rindiņa, nu jau lēnām pietiek.\n")
